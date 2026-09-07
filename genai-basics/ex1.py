f3903014f75e4d937e1e2187083559071269798f
rt16apr1994 <108141781+rt16apr1994@users.noreply.github.com>
2026-09-08 02:53:19 +0530

    Remove API key from ex1.py

--- genai-basics/ex1.py
+++ genai-basics/ex1.py
@@ -1,6 +1,6 @@
 from google import genai
 
-client=genai.Client(api_key="AQ.Ab8RN6LY9Wdg2jlSvcc7l1pw9n60jRfR-UgjgoDcFJEMr2vsCw")
+client=genai.Client(api_key="")
 question=input("Ask a question: ")
 resp=client.models.generate_content(model="gemini-2.5-flash", contents=question)
 print(resp.text)
